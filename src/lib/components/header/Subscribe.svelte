6986379815cd8d43ab0f9f1fad1af97581bd8fff
JCEEE <0xjceee@proton.me>
2026-02-13 15:45:35 +0000

    create subscribe component fr dialog

---
+++ src/lib/components/header/Subscribe.svelte
@@ -0,0 +1,45 @@
+<div class="content-grid drop-shadow-xl">
+	<article class="grid place-content-center">
+		<div class="subscribe-card rounded-2xl border-2 bg-prime-100 p-12">
+			<h2>Register your interest</h2>
+			<form id="subscribe">
+				<div class="flex flex-col gap-4">
+					<div class="">
+						<label for="username" class="block text-sm/6 font-light">Nickname</label>
+
+						<div
+							class="flex items-center overflow-hidden rounded-md bg-pop-200 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<input
+								id="username"
+								type="text"
+								name="username"
+								placeholder="violet67"
+								class="block min-w-0 grow bg-pop-200 py-1.5 pr-3 pl-1 font-light text-base placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+							/>
+						</div>
+					</div>
+					<div class="">
+						<label for="username" class="block text-sm/6 font-light">Email</label>
+
+						<div
+							class="flex items-center overflow-hidden rounded-md bg-pop-200 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<input
+								id="email"
+								type="email"
+								name="email"
+								placeholder="lfg@mail.com"
+								class="block min-w-0 grow bg-pop-200 py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+							/>
+						</div>
+					</div>
+					<button class="inline-button border-2 bg-wash-100 rounded-lg py-2 px-8"
+						>Sign me up!</button
+					>
+					<!-- <button class="rounded-md bg-wash-200 p-2" type="submit"> Sign me up </button> -->
+				</div>
+			</form>
+		</div>
+	</article>
+</div>
