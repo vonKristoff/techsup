a7429a31e0612a87d2560647c548ac3abcb73f33
JCEEE <0xjceee@proton.me>
2026-06-25 09:26:40 +0100

    improve seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
+import seoFiles from 'vite-plugin-seo-files';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import meta from './src/meta.json';
+const { url } = meta;
 
 export default defineConfig(({ mode }) => ({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), seoFiles({ siteUrl: url })],
 	build: {
 		minify: mode === 'production',
 		sourcemap: mode === 'development'
